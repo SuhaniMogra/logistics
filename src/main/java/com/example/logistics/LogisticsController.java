b4c2415329ef38471de5f7dbb23c134dffd3f5df
Suhani <suhanimogra2004@gmail.com>
2026-02-13 14:42:56 +0530

    Initial logistics project

---
+++ src/main/java/com/example/logistics/LogisticsController.java
@@ -0,0 +1,13 @@
+package com.example.logistics;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogisticsController {
+
+    @GetMapping("/")
+    public String home() {
+        return "🚚 Logistics Service Running Successfully";
+    }
+}
---
+++ src/test/java/com/example/logistics/LogisticsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.logistics;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsApplicationTests {
+
+	@Test
+	void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
